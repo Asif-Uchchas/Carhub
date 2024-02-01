8363ddf3ff953d33794f84409c3cb2d261c59512
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-02-02 02:33:44 +0600

    Started developing a car website using  NextJS
    Initiated the hero section and finalized the layout of the landing page

---
+++ app/index.ts
@@ -0,0 +1,13 @@
+import Hero from "@/Components/Hero";
+import CustomButton from "@/Components/CustomButton";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    
+
+};
