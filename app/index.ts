573b6ec32e00fc7933d9ddfa35eeac5c5cef41ca
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-02-04 01:59:01 +0600

    Started the car catalogue part but too tired to finish  it completeley

--- app/index.ts
+++ app/index.ts
@@ -2,12 +2,14 @@ import Hero from "@/Components/Hero";
 import CustomButton from "@/Components/CustomButton";
 import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
+// import SearchBar from "@/Components/SearchBar";
+// import CustomFilter from "@/Components/CustomFilter";
 
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer,
-    
-
+    // SearchBar,
+    // CustomFilter,
 };
